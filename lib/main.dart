4173ccbcbdea8f7a9e4cccc467be230935ac1a4e
gabriel santos Vieira <GabrielsantosV2@hotmail.com>
2023-05-22 22:57:35 -0300

    inserção do modal de resultado

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -29,6 +31,12 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController _textEditingControllerAlcool = TextEditingController();
+  TextEditingController _textEditingControllerGasolina =
+      TextEditingController();
+
+  var resultado;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,12 +63,14 @@ class _HomeState extends State<Home> {
                   Padding(
                     padding: EdgeInsets.only(top: 30),
                     child: TextField(
+                      controller: _textEditingControllerAlcool,
                       decoration: InputDecoration(
                           labelText: 'Preço do álcool, ex: 1.59',
                           labelStyle: TextStyle(fontSize: 20)),
                     ),
                   ),
                   TextField(
+                    controller: _textEditingControllerGasolina,
                     decoration: InputDecoration(
                         labelText: 'Preço da gasolina, ex: 3.15',
                         labelStyle: TextStyle(fontSize: 20)),
@@ -68,11 +78,60 @@ class _HomeState extends State<Home> {
                   Padding(
                       padding: EdgeInsets.only(top: 10),
                       child: ElevatedButton(
-                        onPressed: () {},
-                        child: Text('Calcular'),
+                        onPressed: () {
+                          _calculaAbastecimento();
+                          showModalBottomSheet<void>(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return SizedBox(
+                                height: 100,
+                                child: Center(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Container(
+                                        child: Column(
+                                          children: [
+                                            Row(
+                                              children: [
+                                                Center(
+                                                  child: Text(
+                                                    '$resultado',
+                                                    style: TextStyle(
+                                                        fontSize: 20,
+                                                        fontWeight:
+                                                            FontWeight.w700),
+                                                  ),
+                                                ),
+                                              ],
+                                            )
+                                          ],
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        },
+                        child: const Text('Calcular'),
                       )),
                 ],
               ),
             )));
+  }
+
+  _calculaAbastecimento() {
+    setState(() {
+      double precoAlcool = double.parse(_textEditingControllerAlcool.text);
+      double precoGasolina = double.parse(_textEditingControllerGasolina.text);
+      if (precoAlcool / precoGasolina >= 0.7) {
+        resultado = 'Indicado por gasolina';
+      } else {
+        resultado = 'Indicado por álcool';
+      }
+    });
   }
 }
